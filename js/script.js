d2edca5cd15ceb5b38abc9492e9395a786671d75
Ashley Swank <Ashley@Psychics-MacBook-Pro.local>
2021-11-09 20:15:00 -0500

    finished

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 
 
-document.addEventListener('keydown', function(e) {
-    console.log(e.keyCode)
-})
+
+function keyHit(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; 
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+
+document.addEventListener('keydown', keyHit);
